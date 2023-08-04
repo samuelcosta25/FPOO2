1e85c33dfde0d2b62eaf655fbc99e37d72894811
samuelcosta25 <samu.live@outlook.com>
2023-08-03 21:54:19 -0300

    03.08.23

--- Exception/ExemploThrow.java
+++ Exception/ExemploThrow.java
@@ -6,11 +6,16 @@ public class ExemploThrow {
     public static void main(String[] args) {
         boolean loop=true;
         while (loop) {
+        String login = JOptionPane.showInputDialog("Informe um Login");
         String senhaDigitada = JOptionPane.showInputDialog("Informe uma Senha de 6 Digitos");
+        String login = JOptionPane.showInputDialog("Informe um Login");
         try {
+            if (senhaDigitada.equals(login)){
+                throw new Exception("A senha não pode ser igual ao Login");
+            }
             if (senhaDigitada.length()!=6) {
                 throw new Exception("Senha inválida");
-            }
+            } 
             loop=false;
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e.getMessage());
