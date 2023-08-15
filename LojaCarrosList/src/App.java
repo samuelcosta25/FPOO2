6fa4b6ad85bfe3428cfc421f85bcfc85c712f38b
samuelcosta25 <samu.live@outlook.com>
2023-08-14 21:26:28 -0300

    14.08.23

--- LojaCarrosList/src/App.java
+++ LojaCarrosList/src/App.java
@@ -1,14 +1,50 @@
-package LojaCarrosList.src;
-
 import java.util.*;
 
+import javax.swing.JOptionPane;
+
 public class App {
-    public static void main(String[] args) {
-        
-        List <Carros> listaCarros = new ArrayList<>();
-        //1. Cadastrar carro
-        //2. Listar carro
-        //3. Consultar carro
-        //4. Apagar carro
+    public static void main(String[] args) throws Exception {
+        //classe carros com metodos e atributos
+        //App
+        List<Carros> listaCarros = new ArrayList<>();
+        boolean navegar = true;
+        while (navegar) {
+            int acao = Integer.parseInt(JOptionPane.showInputDialog("1- Cadastrar Carro \n2- Listar Carros\n3- Consultar Carro\n4- Apagar Carro"));
+             
+            if (acao == 1){ // cadastrar carro
+                //criar um objteto
+                String marcaCarro = JOptionPane.showInputDialog("Marca do carro: ");
+                String modeloCarro = JOptionPane.showInputDialog("Modelo do carro: ");
+                String corCarro = JOptionPane.showInputDialog("Cor do carro: ");
+                String anoCarro = JOptionPane.showInputDialog("Ano do carro: ");
+                Carros car = new Carros(marcaCarro, modeloCarro, corCarro, anoCarro);
+                listaCarros.add(car);
+
+            } else if (acao==2) { //Listar carros cadasrados
+               String listar="";
+                for (Carros carros : listaCarros) {
+                    listar += carros.imprimirLN();
+                }
+                JOptionPane.showMessageDialog(null, listar);
+            } else if (acao==3) { //Consultar carros
+                String consultar="";
+                int i=0;
+                for (Carros carros : listaCarros) {
+                    consultar+= i+carros.imprimirLN();
+                    i++;
+                }
+                int acao2 = Integer.parseInt(JOptionPane.showInputDialog("Escolha o carro: \n"+consultar));
+                listaCarros.get(acao2).imprimir();
+            } else if (acao == 4){
+                String excluir="";
+                int acao2 = Integer.parseInt(JOptionPane.showInputDialog("Escolha o carro a ser excluído: \n"+excluir));
+                listaCarros.get(acao2).imprimir();
+                listaCarros.remove(acao2);
+            }
+        }
+        //1- Cadastrar Carros na lista
+        //2- Listar Carros cadastrados
+        //3- Consultar um Carro cadastrado
+        //4- Apagar Carro cadastrado
     }
 }
--- LojaCarrosList/src/Carros.java
+++ LojaCarrosList/src/Carros.java
@@ -1,47 +1,65 @@
-package LojaCarrosList.src;
+import javax.swing.JOptionPane;
 
 public class Carros {
-    //atrib
-    String marca;
     String modelo;
-    String ano;
     String cor;
+    String marca;
+    String ano;
 
-    //method
-    //construtor - 1 cheio e 1 vazio
-    public Carros(String marca, String modelo, String ano, String cor) {
-        this.marca = marca;
+    // metodos
+    // construtor cheio
+    public Carros(String marca, String modelo, String cor, String ano) {
         this.modelo = modelo;
-        this.ano = ano;
         this.cor = cor;
+        this.marca = marca;
+        this.ano = ano;
     }
-    
+
+    // construtor vazio
     public Carros() {
     }
 
-      //getters and setters
-    public String getMarca() {
-        return marca;
-    }
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
+    // getters and setters
     public String getModelo() {
         return modelo;
     }
+
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
-    public String getAno() {
-        return ano;
-    }
-    public void setAno(String ano) {
-        this.ano = ano;
-    }
+
     public String getCor() {
         return cor;
     }
+
     public void setCor(String cor) {
         this.cor = cor;
-    }        
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    //metodo proprio
+        public String imprimirLN() {
+            String imprimir = " "+marca+" "+modelo+" "+ano+" "+cor+"\n";
+            return imprimir;
+        }
+
+        public void imprimir(){
+            JOptionPane.showMessageDialog(null, marca+"\n "+modelo+"\n "+ano+"\n "+cor);
+        }
+
 }
